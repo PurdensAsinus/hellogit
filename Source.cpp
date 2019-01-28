d908253d04741466c7d54cedc1d6e1c36b7fb14b
PurdensAsinus <PurdensAsinus@users.noreply.github.com>
2019-01-28 19:02:28 -0400

    Added source file

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include <string>
+. . .
+// declarations
+string str1 = "hello";
+string str2 = str1 + " world";
+
+// printing
+cout<< "str1 = " << str1 << endl;
+cout<<"the 4th character is "<<str1[3]<<endl;
+cout<<"str1 has "<<str1.size()<<" characters"<<endl;
+
+// comparison coperators
+if (str1=="hello")
+    cout<<"hi there"<<endl;
+if (str2!="world")
+    cout<<"out of this world"<<endl;
+
+// appending to a string
+str2+="ly one";  //or str2 = str2 + "ly one!!"
